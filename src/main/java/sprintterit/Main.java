dae94ae62e607f40624cefd197bac233608a883a
Matti Riekkinen <matti.riekkinen@icloud.com>
2017-03-29 17:12:22 +0300

    Hello World

--- src/main/java/sprintterit/Main.java
+++ src/main/java/sprintterit/Main.java
@@ -1,7 +1,34 @@
+package sprintterit;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import spark.ModelAndView;
 import static spark.Spark.*;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
 public class Main {
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+    
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        // Osoite: http://osoitetuleetahan.herokuapp.com/
+        // Paikallinen versio: http://localhost:4567/
+
+        // Asetetaan portti, jos heroku antaa PORT-ympäristömuuttujan
+        if (System.getenv("PORT") != null) {
+            port(Integer.valueOf(System.getenv("PORT")));
+        }
+
+        // Käytetään oletuksena paikallista sqlite-tietokantaa
+        // Jos heroku antaa käyttöömme tietokantaosoitteen, otetaan se käyttöön
+        String jdbcOsoite = "jdbc:sqlite:tietokannat/tietokantatiedosto.db";
+        if (System.getenv("DATABASE_URL") != null) {
+            jdbcOsoite = System.getenv("DATABASE_URL");
+        }
+
+        get("/", (req, res) -> {
+            HashMap map = new HashMap<>();
+            map.put("title", "Viitteidenhallinta");
+
+            return new ModelAndView(map, "index");
+        }, new ThymeleafTemplateEngine());
     }
 }
